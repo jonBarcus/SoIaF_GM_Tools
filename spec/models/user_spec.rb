504b6f600137d56d8afea3256f3c737ed95b5786
jonBarcus <barcus.j@gmail.com>
2015-03-18 19:07:38 -0400

    Continuing to write tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe User do
     user = User.new({first_name: "Steven"})
     expect(user.first_name).to eq("Steven")
   end
+  it "has a last name" do
+    user = User.new({last_name: "Stevenson"})
+    expect(user.last_name).to eq("Stevenson")
+  end
 end
