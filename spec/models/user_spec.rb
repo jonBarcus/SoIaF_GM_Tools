68419f532ffeb3bfb06b570c3edd790938d52966
jonBarcus <barcus.j@gmail.com>
2015-03-18 19:14:51 -0400

    Finished User Model tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User do
+  let(:user) {User.new({first_name: "Steven",
+                        last_name:  "Stevenson",
+                        email:      "test@test.com",
+                        is_admin:   false}
+                        )}
+
   it "has a first name" do
-    user = User.new({first_name: "Steven"})
     expect(user.first_name).to eq("Steven")
   end
   it "has a last name" do
-    user = User.new({last_name: "Stevenson"})
     expect(user.last_name).to eq("Stevenson")
+  end
+  it "has an e-mail address" do
+    expect(user.email).to eq("test@test.com")
+  end
+  it "is not an admin" do
+    expect(user.is_admin).to be_falsy
   end
 end
