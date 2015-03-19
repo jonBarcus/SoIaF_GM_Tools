9d2b5bb59d97b2771bef1eaf977ff5386c74ba06
jonBarcus <barcus.j@gmail.com>
2015-03-18 20:03:30 -0400

    Created Region migration and ran rake db:migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318230244) do
+ActiveRecord::Schema.define(version: 20150319000209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "regions", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
