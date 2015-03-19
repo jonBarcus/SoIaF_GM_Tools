2a289d97fb091c63696dabf9de9f7fa386c17e2f
jonBarcus <barcus.j@gmail.com>
2015-03-19 11:30:00 -0400

    Started two things - Added Regions to seeds file - Added Welcome to routes file and then created welcome_controller and welcome view.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Region.delete_all
+
+the_north = Region.create({name:       "The North"})
+the_vale = Region.create({name:        "The Vale"})
+the_riverlands = Region.create({name:  "The Riverlands"})
+the_crownlands = Region.create({name:  "The Crownlands" })
+the_reach = Region.create({name:       "The Reach"})
+the_stormlands = Region.create({name:  "The Stormlands"})
+dorne = Region.create({name:           "Dorne"})
+the_seven_kingdoms.create({name:       "The Seven Kingdoms"})
