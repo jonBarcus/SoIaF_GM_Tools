6e538a0ad1613b6057c3576f98fdc4c61abeba56
jonBarcus <barcus.j@gmail.com>
2015-03-19 16:48:35 -0400

    Account Creation/Log In/Out Successfully implemented session creation/destruction as well as account creation.  Verifyed log in is working as well as log out.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  # this is for bcrypt (makes use of password/password confirmation)
+  # It also provides the password digest in the database
   has_secure_password
 
 
