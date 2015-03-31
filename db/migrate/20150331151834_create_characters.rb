74123c7f71884d5f23928084ced6f056eb485f1c
jonBarcus <barcus.j@gmail.com>
2015-03-31 14:28:10 -0400

    Characters Migration

---
+++ db/migrate/20150331151834_create_characters.rb
@@ -0,0 +1,14 @@
+class CreateCharacters < ActiveRecord::Migration
+  def change
+    create_table :characters do |t|
+      t.string        :name
+      t.string        :surname
+      t.integer       :gender_id
+      t.integer       :house_id
+      t.text          :description
+      t.text          :history
+      t.integer       :user_id
+      t.timestamps
+    end
+  end
+end
