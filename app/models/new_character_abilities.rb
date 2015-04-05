cc583aa5e0c28f4365f697ce92b20b666374bbf4
jonBarcus <barcus.j@gmail.com>
2015-04-04 20:31:19 -0400

    Started work on ability_gen_module for new character creation.  Need to update the seeds file with all the ability names first.

---
+++ app/models/new_character_abilities.rb
@@ -0,0 +1,12 @@
+# This module will run upon NEW CHARACTER creation.
+# Its function is to create all the abilities with the
+# default value of 2, regardless of any of the other
+# character attributes (i.e. age, gender, etc.)
+
+module NewCharacterAbilities(id)
+
+  ability_names = AbilityNames.all
+
+
+
+end
