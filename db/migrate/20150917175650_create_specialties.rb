f5f5f2cb117ccc1ce1c949fa369cf9faa1c3ae64
jonBarcus <barcus.j@gmail.com>
2015-09-17 14:09:13 -0400

    completed specialty migration, will now create and finish specialty names migration

--- db/migrate/20150917175650_create_specialties.rb
+++ db/migrate/20150917175650_create_specialties.rb
@@ -1,6 +1,10 @@
 class CreateSpecialties < ActiveRecord::Migration
   def change
     create_table :specialties do |t|
+      t.integer    :character_id
+      t.integer    :specialty_name_id
+      t.integer    :ability_id
+      t.timestamps
     end
   end
 end
