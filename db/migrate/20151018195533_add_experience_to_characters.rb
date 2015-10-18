a64c049b6466dc4717d0be277a46000f377e6df7
jonBarcus <barcus.j@gmail.com>
2015-10-18 16:57:22 -0400

    this is annoying

--- db/migrate/20151018195533_add_experience_to_characters.rb
+++ db/migrate/20151018195533_add_experience_to_characters.rb
@@ -1,5 +1,5 @@
 class AddExperienceToCharacters < ActiveRecord::Migration
   def change
-    add_column  :characters, :available_experience, :integer
+    add_column  :characters, :experience, :integer
   end
 end
