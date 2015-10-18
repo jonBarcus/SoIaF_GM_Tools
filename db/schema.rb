953b02f415096d310411fe609b5e7cb26c8affce
jonBarcus <barcus.j@gmail.com>
2015-10-18 17:26:24 -0400

    ran rake db:migrate, seemingly successful this time.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018195533) do
+ActiveRecord::Schema.define(version: 20151018212501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20151018195533) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "experience"
   end
 
   create_table "genders", force: :cascade do |t|
